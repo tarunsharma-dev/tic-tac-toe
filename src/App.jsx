32df21ef13af0acb88b2beacb37e639bac56a99d
Tarun Sharma <tarunsharma@192.168.0.60>
2025-01-08 17:08:39 +0530

    tic tac toe game

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+import TicTacToe from "./TicTacToe/TicTacToe";
+function App() {
+  return (
+    <div className="min-h-screen bg-[#0f1b21] flex justify-center items-center">
+      <TicTacToe />
+    </div>
+  );
+}
+
+export default App;
