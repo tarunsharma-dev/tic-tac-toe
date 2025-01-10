82a85468b7fe939b8ee06247370538a9cedbceb8
Tarun Sharma <tarunsharma@192.168.1.6>
2025-01-10 10:57:06 +0530

    feature: tic tac toe game mobile responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import "./App.css";
 import TicTacToe from "./TicTacToe/TicTacToe";
+import { Analytics } from "@vercel/analytics/react";
 function App() {
   return (
-    <div className="min-h-screen bg-[#0f1b21] flex justify-center items-center">
-      <TicTacToe />
-    </div>
+    <>
+      <Analytics />
+
+      <div className="min-h-screen bg-[#0f1b21] flex justify-center items-center">
+        <TicTacToe />
+      </div>
+    </>
   );
 }
 
